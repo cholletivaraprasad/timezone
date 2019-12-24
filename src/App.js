46ad83958e9b0f9b9d6f64810a30f21f197f2674
cholleti varaprasad <cholleti.cena@gmail.com>
2019-12-24 13:13:52 +0530

    added timezone api

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import DisplayMap from './components/DisplayMap';
+function App() {
+  return (
+    <div>
+     <DisplayMap/>
+    </div>
+  );
+}
+
+export default App;
